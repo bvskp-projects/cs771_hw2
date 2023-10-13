45fcc63e2ac1b8c2a0675f0e0b566d2b385e22a8
msmink01 <94126155+msmink01@users.noreply.github.com>
2023-10-13 14:31:04 -0500

    Update student_code.py

--- code/student_code.py
+++ code/student_code.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.autograd import Function
 from torch.nn.modules.module import Module
 from torch.nn.functional import fold, unfold
